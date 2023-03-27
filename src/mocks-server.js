56f54c30341b3da70240abde7a13601bd42eafe3
Aaron Buckley <aaron.buckley@springernature.com>
2023-03-27 16:30:16 +0100

    add close and open server, return promises and add new typings

--- src/mocks-server.js
+++ src/mocks-server.js
@@ -4,25 +4,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const startMockingServer = (options) => {
-	if (!options) {
-		throw new Error('You must supply options for the configuration');
-	}
-	if (!options.mocksPath && !options.mockData) {
-		throw new Error('You must supply a mocksPath or mockData');
-	}
-	const app = express();
+	return new Promise((resolve, reject) => {
+		if (!options) {
+			throw new Error('You must supply options for the configuration');
+		}
+		if (!options.mocksPath && !options.mockData) {
+			throw new Error('You must supply a mocksPath or mockData');
+		}
+		const app = express();
 
-	app.use(bodyParser.urlencoded({extended: false}));
-	app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({extended: false}));
+		app.use(bodyParser.json());
 
-	app.use(logRequest());
+		app.use(logRequest());
 
-	app.use(returnMock(options));
+		app.use(returnMock(options));
 
-	const port = process.env.PORT ?? 5010;
-	app.listen(port, () => {
-		console.info(`🎬 server started on http://localhost:${port}`);
+		const port = process.env.PORT ?? 5010;
+		const instance = app.listen(port, error => {
+			if (error) {
+				return reject(error);
+			}
+			console.info(`🎬 server started on http://localhost:${port}`);
+
+			resolve(instance);
+		});
 	});
 };
 
-module.exports = {startMockingServer};
+function closeMockingServer(server) {
+	return new Promise((resolve, reject) => {
+		server.close(error => {
+			if (error) {
+				return reject(error);
+			}
+			resolve();
+		});
+	});
+}
+
+module.exports = {startMockingServer, closeMockingServer};
--- types/mocks-server.d.ts
+++ types/mocks-server.d.ts
@@ -1,1 +1,10 @@
-export function startMockingServer(options: any): void;
+import * as http from 'http';
+
+export interface MocksServerOptions<T> {
+	port: number;
+	mocksPath: string;
+	mockData: T;
+}
+
+export function startMockingServer<T>(options: MocksServerOptions<T>): Promise<http.Server>;
+export function stopMockingServer(server: http.Server): Promise<void>;
